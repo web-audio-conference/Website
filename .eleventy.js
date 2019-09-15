a768407d9b89a59719a28e9885dd39a84ce53101
MeltingPlanet <varpe1992@gmail.com>
2019-09-15 13:15:19 +0200

    y

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,57 @@
+const bibtexParse = require('bibtex-parse');
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(config) {
-  config.addCollection('posts', collection => {
-    return collection.getFilteredByGlob('src/posts/*.md');
+  let bibs = 'src/_data/papers/bib';
+  let mdPath = 'src/posts/';
+  let title, abstract;
+
+  bibFiles = fs.readdirSync(bibs);
+
+  bibFiles.forEach(function(file) {
+    newFileName = file.slice(0, -4);
+
+    let bib = fs.readFileSync(path.join(bibs, file), 'utf8');
+    let parsed = bibtexParse.parse(bib);
+
+    /*     fs.writeFileSync(
+      path.join(jsons, newFileName + '.11tydata.json'),
+      JSON.stringify(parsed),
+      'utf8'
+    ); */
+
+    title = parsed.entries[0].properties.title.value.slice(1, -1);
+    if (title.includes('{\\&}')) {
+      title = title.replace(/{\\&}/gi, '&');
+    }
+    abstract = parsed.entries[0].properties.abstract.value;
+    if (abstract.includes('{\\&}')) {
+      abstract = abstract.replace(/{\\&}/gi, '&');
+    }
+
+    fs.writeFileSync(
+      path.join(mdPath, newFileName + '.md'),
+      '---\ntitle: "' +
+        title +
+        '"' +
+        '\nabstract: "' +
+        abstract +
+        '"' +
+        '\ntags: ' +
+        'year' +
+        newFileName.slice(0, 4) +
+        '\n---'
+    );
   });
 
-  config.addPassthroughCopy('src/assets');  // To pass through css, images and scripts to output folder (dist)
+  config.addCollection('posts', collection => {
+    return collection.getFilteredByGlob(['src/posts/*.md']);
+  });
+
+  config.addPassthroughCopy('src/assets'); // To pass through css, images and scripts to output folder (dist)
+
+  config.addPassthroughCopy('src/_data/papers');
 
   return {
     dir: {
--- src/proceedings.md
+++ src/proceedings.md
@@ -4,4 +4,14 @@ title: 'Proceedings'
 date: '2019-05-23'
 ---
 
-Integer sit amet neque eros. Curabitur ut mi at risus condimentum luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi quis tellus a dui feugiat porta sit amet vitae ante. Vestibulum sed mi eget sapien auctor malesuada eget sit amet dolor. Pellentesque vitae feugiat est. Fusce consectetur purus sed magna volutpat, eu mollis orci accumsan. Praesent semper velit vel tellus finibus, at auctor ex feugiat. Fusce tristique massa quam. Etiam aliquam ex nisl, ut ultrices ligula blandit at. Donec vel posuere lectus, nec ultrices enim.
+# 2015
+
+{% for post in collections.year2015 %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endfor %}
+
+# 2016
+
+{% for post in collections.year2016 %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endfor %}

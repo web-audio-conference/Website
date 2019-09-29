6915dc972f0362b549d52a6161010eb7696e94de
MeltingPlanet <varpe1992@gmail.com>
2019-09-30 00:44:32 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,23 @@
-const bibtexParse = require('bibtex-parse');
-const fs = require('fs');
-const path = require('path');
-//const parser = new (require('simple-excel-to-json')).XlsParser();
-const excel2json = require('xls-to-json');
+const bibtexParse = require("bibtex-parse");
+const fs = require("fs");
+const path = require("path");
+const excel2json = require("xls-to-json");
 
 module.exports = function (config) {
-  let bibs = 'src/_data/papers/bib/';
-  let excels = 'src/_data/papers/excel/';
-  let mdPath = 'src/posts/';
-  let jsonPath = 'src/_data/papers/json/';
-  var title,
-    abstract,
-    booktitle,
-    pages,
-    author,
-    editor,
-    year,
-    month,
-    publisher,
-    address,
-    issn,
-    id;
+  let bibs = "src/_data/papers/bib/";
+  let excels = "src/_data/papers/excel/";
+  let mdPath = "src/posts/";
+  let jsonPath = "src/_data/papers/json/";
+
   /*
   // Parsing excel sheet 1 and writing md files from it
-  excel = path.join(excels, 'CameraReadyPapers2017.xls');
+  excel = path.join(excels, "CameraReadyPapers2017.xls");
 
   excel2json(
     {
       input: excel, // input xls
       output: jsonPath, // output json
-      sheet: 'Performances and Artworks', // specific sheetname
+      sheet: "Performances and Artworks", // specific sheetname
       rowsToSkip: 2 // number of rows to skip at the top of the sheet; defaults to 0
     },
     function(err, result) {
@@ -38,23 +26,23 @@ module.exports = function (config) {
       } else {
         
         fs.writeFileSync(
-          path.join(jsonPath, 'test.json'),
+          path.join(jsonPath, "test.json"),
           JSON.parse(result),
-          'utf8');
+          "utf8");
         
         //console.log(result.length);
         for (let x in result) {
           fs.writeFileSync(
-            path.join(mdPath, `2017_${result[x]['Paper ID']}.md`),
-            '---\ntitle: "' +
-              result[x]['Paper Title'] +
-              '"' +
-              '\nabstract: "' +
-              result[x]['Abstract'] +
-              '"' +
-              '\ntags: ' +
-              'year2017' +
-              '\n---'
+            path.join(mdPath, `2017_${result[x]["Paper ID"]}.md`),
+            "---\ntitle: "" +
+              result[x]["Paper Title"] +
+              """ +
+              "\nabstract: "" +
+              result[x]["Abstract"] +
+              """ +
+              "\ntags: " +
+              "year2017" +
+              "\n---"
           );
         }
       }
@@ -65,24 +53,30 @@ module.exports = function (config) {
   bibFiles = fs.readdirSync(bibs);
 
   bibFiles.forEach(function (file) {
+
     year = file.slice(3, 6);
 
-    let bib = fs.readFileSync(path.join(bibs, file), 'utf8');
+    let bib = fs.readFileSync(path.join(bibs, file), "utf8");
     let parsed = bibtexParse.parse(bib);
     fs.writeFileSync(
-      path.join(jsonPath, 'bibtojson.json'),
+      path.join(jsonPath, "bibtojson.json"),
       JSON.stringify(parsed),
-      'utf8'
+      "utf8"
     );
 
     for (let i in parsed.entries) {
+      let title, abstract, booktitle, pages, author, editor, year, month, publisher, address, issn, id;
+
       abstract = parsed.entries[i].properties.abstract.value;
+      if (abstract.includes('"')) {
+        abstract = abstract.replace(/"/g, "'");
+      }
       address = parsed.entries[i].properties.address.value;
       author = parsed.entries[i].properties.author.value;
       booktitle = parsed.entries[i].properties.booktitle.value;
       editor = parsed.entries[i].properties.editor.value;
       month = parsed.entries[i].properties.booktitle.value;
-      if (parsed.entries[i].properties.hasOwnProperty('pages')) {
+      if (parsed.entries[i].properties.hasOwnProperty("pages")) {
         pages = parsed.entries[i].properties.pages.value;
       } else {
         pages = "";
@@ -96,12 +90,12 @@ module.exports = function (config) {
 
 
       data = `--- 
-  title: "${title.slice(1, -1)}" 
-  abstract: "abstract here" 
+  title: "${title}" 
+  abstract: "${abstract}" 
   address: "${address}" 
-  author: "" 
+  author: "${author}" 
   booktitle: "${booktitle}" 
-  editor: "" 
+  editor: "${author}" 
   month: "${month}"
   pages: "${pages}" 
   publisher: "${publisher}" 
@@ -116,32 +110,32 @@ module.exports = function (config) {
     }
     /*
     title = parsed.entries[0].properties.title.value.slice(1, -1);
-    if (title.includes('{\\&}')) {
-      title = title.replace(/{\\&}/gi, '&');
+    if (title.includes("{\\&}")) {
+      title = title.replace(/{\\&}/gi, "&");
     }
     abstract = parsed.entries[0].properties.abstract.value;
-    if (abstract.includes('{\\&}')) {
-      abstract = abstract.replace(/{\\&}/gi, '&');
+    if (abstract.includes("{\\&}")) {
+      abstract = abstract.replace(/{\\&}/gi, "&");
     }
     */
   });
 
-  config.addCollection('posts', collection => {
-    return collection.getFilteredByGlob(['src/posts/*.md']);
+  config.addCollection("posts", collection => {
+    return collection.getFilteredByGlob(["src/posts/*.md"]);
   });
 
-  config.addPassthroughCopy('src/assets'); // To pass through css, images and scripts to output folder (dist)
+  config.addPassthroughCopy("src/assets"); // To pass through css, images and scripts to output folder (dist)
 
-  config.addPassthroughCopy('src/_data/papers');
+  config.addPassthroughCopy("src/_data/papers");
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist"
     },
-    templateFormats: ['html', 'md', 'css', 'liquid'],
-    htmlTemplateEngine: 'liquid',
-    markdownTemplateEngine: 'liquid',
+    templateFormats: ["html", "md", "css", "liquid"],
+    htmlTemplateEngine: "liquid",
+    markdownTemplateEngine: "liquid",
     passthroughFileCopy: true
   };
 };
--- src/proceedings.md
+++ src/proceedings.md
@@ -18,6 +18,58 @@ date: '2019-05-23'
 
 # 2017
 
+## Papers
+
 {% for post in collections.year2017 %}
+{% if post.data.type == 'Paper' %}
 <a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Posters
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Poster' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Demos
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Demo' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Performances
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Performance' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Talks
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Talk' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Posters
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Poster' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
+{% endfor %}
+
+## Artworks
+
+{% for post in collections.year2017 %}
+{% if post.data.type == 'Artwork' %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endif %}
 {% endfor %}

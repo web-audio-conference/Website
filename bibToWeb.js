383c891a74a4c90216249289e9b3cb2ab6ab3a6e
Jan Monschke <janmonschke@fastmail.com>
2019-12-04 23:30:35 +0100

    remove unused code

--- bibToWeb.js
+++ bibToWeb.js
@@ -1,7 +1,6 @@
 const bibtexParse = require("bibtex-parse");
 const fs = require("fs");
 const path = require("path");
-const pdfNumPg = require("./pdfNumPg.js");
 
 let mdPath = "src/posts/";
 let jsonPath = "src/_data/papers/json/";
@@ -99,40 +98,6 @@ module.exports = function(bibs) {
       } else {
         media = "none";
       }
-      /*
-      if (parsed[i].type == "inproceedings") {
-        try {
-          pdfNumPg(year, id).then(pages => {
-            data = `--- 
-title: "${title}" 
-abstract: "${abstract}" 
-address: "${address}" 
-author: "${author}"
-webAuthor: "${webAuthor}" 
-booktitle: "${booktitle}" 
-editor: "${editor}" 
-month: "${month}"
-pages: "1-${pages}" 
-publisher: "${publisher}" 
-series: "${series}"
-track: "${track}"  
-year: "${year}" 
-id: "${id}" 
-tags: year${year}
-media: ${media} 
-pdflink: ${pdflink}
-ISSN: 2663-5844
----`;
-
-            fs.writeFileSync(path.join(mdPath, `${id}.md`), data);
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      } else {
-        pages = "";
-      }
-*/
       const data = `--- 
 title: "${title}" 
 abstract: "${abstract}" 
